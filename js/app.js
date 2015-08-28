4fe68fc34abaf7080f241e75f38d54c2b2a2e328
johnhur <johnjhur@gmail.com>
2015-08-27 18:42:32 -0700

    partial paths

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ var app = angular.module("shoppingCart", ['ngRoute']);
 app.config(function($routeProvider, $locationProvider){
 	$routeProvider
 	.when('/', {
-		templateUrl:'../partials/about.html',
+		templateUrl:'partials/about.html',
 		controller: 'homeController'
 	})
 	.when('/contact', {
-		templateUrl:'../partials/contact.html',
+		templateUrl:'partials/contact.html',
 		controller: 'homeController'
 	})
 	.when('/home', {
-		templateUrl:'../partials/home.html',
+		templateUrl:'partials/home.html',
 		controller: 'homeController'
 	})
 	.when('/cart', {
-		templateUrl:'../partials/cart.html',
+		templateUrl:'partials/cart.html',
 		controller: 'cartController'
 	})
 })
